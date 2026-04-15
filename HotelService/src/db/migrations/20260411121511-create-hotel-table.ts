82602bf2512ba75ace4fcca6a72496b69cb7bc11
Nikhil Sai Goori <nikhilsai550000@gmail.com>
2026-04-15 12:18:24 +0530

    added the ratings and rating count through hotels table

--- HotelService/src/db/migrations/20260411121511-create-hotel-table.ts
+++ HotelService/src/db/migrations/20260411121511-create-hotel-table.ts
@@ -1,19 +1,19 @@
+import { QueryInterface } from "sequelize"
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface) {
+  async up(queryInterface: QueryInterface) {
     await queryInterface.sequelize.query(`
       CREATE TABLE IF NOT EXISTS hotels(
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         location VARCHAR(255) NOT NULL,
-        rating FLOAT NOT NULL,
         createdAt TIMESTAMP NOT NULL,
         updatedAt TIMESTAMP NOT NULL
       )
     `)
   },
 
-  async down(queryInterface) {
+  async down(queryInterface: QueryInterface) {
     await queryInterface.sequelize.query(`
       DROP TABLE IF EXISTS hotels
     `)
