9d733f31b4844c12428e1efffbcdae8e578ae830
Nikhil Sai Goori <nikhilsai550000@gmail.com>
2026-07-12 10:23:32 +0530

    Fix BookingService git tracking and add files

---
+++ BookingService/src/prisma/client.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from "./generated/prisma/client"
+
+
+export default new PrismaClient();
---
+++ BookingService/src/prisma/migrations/20260712042844_migrationcheck/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `totalGuests` on the `booking` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `booking` DROP COLUMN `totalGuests`;
