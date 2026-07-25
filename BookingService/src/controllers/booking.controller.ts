55a36f083f86e0b9a1ed534fcc1276f0d70be775
Nikhil Sai Goori <nikhilsai550000@gmail.com>
2026-07-25 16:13:07 +0530

    fix : errors in prisma configuration.

--- BookingService/src/controllers/booking.controller.ts
+++ BookingService/src/controllers/booking.controller.ts
@@ -1,23 +1,27 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { createBookingService, confirmBookingService } from "../services/booking.service";
 
 
-
-export const createBookingHandler = async (req: Request, res: Response) => {
-    const booking = await createBookingService(req.body);
-
-
-    res.status(201).json({
-        bookingId: booking.bookingId,
-        idempotencyKey: booking.idempotencyKey
-    });
-
+export const createBookingHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const booking = await createBookingService(req.body);
+        res.status(201).json({
+            bookingId: booking.bookingId,
+            idempotencyKey: booking.idempotencyKey
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
-export const confirmBookingHandler = async (req: Request, res: Response) => {
-    const booking = await confirmBookingService(req.params.idempotencyKey);
-    res.status(200).json({
-        bookingId: booking.id,
-        status: booking.status,
-    });
+export const confirmBookingHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const booking = await confirmBookingService(req.params.idempotencyKey);
+        res.status(200).json({
+            bookingId: booking.id,
+            status: booking.status,
+        });
+    } catch (error) {
+        next(error);
+    }
 }
